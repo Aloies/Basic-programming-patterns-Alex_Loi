c5318705d3620507794dfde00695c32d4fcb3cb1
AlexLoi <alexloiqqqq@gmail.com>
2022-11-21 18:18:52 +0300

    Finished worked with programming patterns

---
+++ src/com/newHardSkill/Patterns/behavioral/visitor/Developer.java
@@ -0,0 +1,9 @@
+package com.newHardSkill.Patterns.behavioral.visitor;
+
+public interface Developer {
+    void create(ProjectClass projectClass);
+
+    void create(Database database);
+
+    void create(Test test);
+}
---
+++ src/com/newHardSkill/Patterns/behavioral/visitor/ProjectElement.java
@@ -0,0 +1,5 @@
+package com.newHardSkill.Patterns.behavioral.visitor;
+
+public interface ProjectElement {
+    void beWritten(Developer developer);
+}
